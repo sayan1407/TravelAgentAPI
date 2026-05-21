8057b7dd4a8032ad2a271ce5cfb7bdf07c01a878
Sayan Saha <sayan199783@gmail.com>
2026-05-21 13:21:59 +0530

    Add Azure service in the middleware

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ origins = [
     "http://127.0.0.1",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://travelagentapi-dtagfjaxbvhybmeg.canadacentral-01.azurewebsites.net"
 ]
 
 app.add_middleware(
